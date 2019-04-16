5581553f6a29e420c5929543508f5483d2f4003a
Matt Berry <matt@codeberry.ca>
2019-04-15 20:40:00 -0400

    fixed css and js paths

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view-engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	res.render('home.ejs');
@@ -32,11 +32,14 @@ app.post('/', (req,res) => {
 
 app.get('/pages/:subpage' , (req,res) => {
 	let subpage = req.params.subpage;
+	let body = req.body.subBody;
 	let title = 'TurboAudit | ' + subpage;
+	let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi praesentium ab error commodi quaerat cupiditate neque nulla sint, non voluptate eius debitis placeat possimus dolorum quibusdam quos temporibus consequatur nesciunt!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eos voluptatibus temporibus tenetur eligendi et vero sit adipisci. Adipisci repudiandae earum enim est voluptas, consectetur animi dolor provident molestiae, dicta.";
 	res.render('subpage.ejs',
 		{
 			subpage: subpage,
-			title: title
+			title: title,
+			text: text
 		}
 	);
 });
