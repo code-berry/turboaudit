57dc8c05d6f9b3ef3dc5d2362871ae94a02e0c30
Matt Berry <matt@codeberry.ca>
2019-04-15 20:05:02 -0400

    add new paths

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view-engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.render('home.ejs');
@@ -18,6 +18,27 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req,res) => {
 	res.render('about.ejs');
+});
+
+app.post('/', (req,res) => {
+	let name = req.body.name;
+	let email = req.body.email;
+	let topic = req.body.topic;
+	let arr = []
+	arr.push([name, email, topic]);
+	console.log(arr);
+	res.redirect('/');
+});
+
+app.get('/pages/:subpage' , (req,res) => {
+	let subpage = req.params.subpage;
+	let title = 'TurboAudit | ' + subpage;
+	res.render('subpage.ejs',
+		{
+			subpage: subpage,
+			title: title
+		}
+	);
 });
 
 app.listen(PORT, ()=> {
